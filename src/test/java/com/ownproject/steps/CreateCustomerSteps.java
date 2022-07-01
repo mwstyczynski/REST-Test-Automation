a530b3c68b69dff0b3c569589cf9e62b8d93e89a
Mateusz Styczynski <mateusz.styczynski@bitpanda.com>
2022-07-01 16:07:33 +0200

    + minor refactor in 1st step implementation

--- src/test/java/com/ownproject/steps/CreateCustomerSteps.java
+++ src/test/java/com/ownproject/steps/CreateCustomerSteps.java
@@ -1,18 +1,15 @@
 package com.ownproject.steps;
 
 import com.ownproject.models.request.CreateCustomerRequest;
-import com.ownproject.models.response.CreateCustomerResponse;
 import io.cucumber.java.en.Given;
 
 public class CreateCustomerSteps extends StepsConfig {
-    
+
     @Given("Create customer request is sent, supplemented with \"{}\" income last year")
     public void sendCreateCustomerRequest(Double income) {
-        scenarioContext.setIncome(income);
         CreateCustomerRequest request = createCustomerSampler.createCustomerWithIncome(income);
         scenarioContext.setCreateCustomerRequest(request);
-        CreateCustomerResponse response = taxCalculationClient.createCustomer(request).as(CreateCustomerResponse.class);
-        scenarioContext.setCreateCustomerResponse(response);
+        scenarioContext.setCreateCustomerResponse(taxCalculationService.createCustomer(request));
     }
 
 
